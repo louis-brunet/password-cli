6b80797fcfbd224328c7dda6f00f27bb10db5928
Louis Brunet <louis.brunet1@grenoble-inp.org>
2023-09-23 16:31:28 +0200

    chore: remove unused, commented code

--- src/encryption.rs
+++ src/encryption.rs
@@ -52,7 +52,6 @@ impl CipherKey {
     }
 }
 
-// TODO: I think this is unused
 impl From<&[u8]> for CipherKey {
     fn from(value: &[u8]) -> Self {
         return Self::new(hmac_sha256::Hash::hash(value));
@@ -70,19 +69,7 @@ pub struct Cipher {
 }
 
 impl Cipher {
-    // pub fn new(key_input: &CipherKeyInput) -> Self {
-    // pub fn new(key_input: &[&[u8]]) -> Self {
     pub fn new(key: &CipherKey) -> Self {
-    // pub fn new(key: &impl AsRef<[u8; 32]>) -> Self {
-        // todo!("finish the refactor of this signature (undo changes ?)");
-        // assert!(!key_input.is_empty());
-        //
-        // let mut key_hasher = hmac_sha256::Hash::new();
-        // for key in key_input {
-        //     key_hasher.update(key);
-        // }
-        // let key_bytes = key_hasher.finalize();
-
         let key: &[u8; 32] = key.as_ref();
         return Self {
             aes: Aes256Gcm::new(key.into()),
@@ -106,65 +93,6 @@ impl Cipher {
         return Ok(plaintext);
     }
 }
-
-// pub fn test() {
-//     // // The encryption key can be generated randomly:
-//     // let key = Aes256Gcm::generate_key(OsRng);
-//     //
-//     // Transformed from a byte array:
-//     // let key: &[u8; 32] = &[42; 32];
-//     // let key: &Key<Aes256Gcm> = key.into();
-//     //
-//     let hashed = hmac_sha256::Hash::hash(&[123u8]);
-//     println!(
-//         "testing hash: {:?} --> (len = {}) {:?}",
-//         123,
-//         hashed.len(),
-//         hashed
-//     );
-//
-//     test_cipher(b"plaintext message");
-//     test_cipher(b"aaaaaaaaaaaaaaaaa");
-//     test_cipher(b"aaaaaaaaaaaaaaaaaaaaaaaaaaa");
-//     test_cipher(b"plaintext messageplaintext messageplaintext messageplaintext messageplaintext messageplaintext messageplaintext message");
-//
-//     // // Note that you can get byte array from slice using the `TryInto` trait:
-//     // let key: &[u8] = &[42; 32];
-//     // let key: [u8; 32] = key.try_into().unwrap();
-//     //
-//     // // Alternatively, the key can be transformed directly from a byte slice
-//     // // (panicks on length mismatch):
-//     // let key = Key::<Aes256Gcm>::from_slice(&key);
-//
-//     // let cipher = Aes256Gcm::new(key);
-//     // let msg = encrypt(og_plaintext, &cipher).expect("encrypt error");
-//     // let plaintext = decrypt(msg.ciphertext.as_slice(), &cipher, &msg.nonce).expect("decrypt error");
-//     //
-//     // let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
-//     // let ciphertext = cipher.encrypt(&nonce, og_plaintext.as_ref()).unwrap();
-//     // let plaintext = cipher.decrypt(&nonce, ciphertext.as_ref()).unwrap();
-// }
-//
-// fn test_cipher(og_plaintext: &[u8]) {
-//     let secret = b"my super secret key";
-//     let cipher = Cipher::new(&secret.as_slice().into());
-//     let msg = cipher.encrypt(og_plaintext).expect("encrypt error");
-//     let plaintext = cipher
-//         .decrypt(&msg.ciphertext, &msg.nonce)
-//         .expect("decrypt error");
-//
-//     assert_eq!(&plaintext, og_plaintext);
-//
-//     println!(
-//         "\ntesting cipher: (len = {} ){:?}\nwith nonce {:?}\n --> (length={}) {:?}\n --> {:?}",
-//         og_plaintext.len(),
-//         og_plaintext,
-//         msg.nonce,
-//         msg.ciphertext.len(),
-//         msg.ciphertext,
-//         String::from_utf8(plaintext).unwrap(),
-//     );
-// }
 
 #[cfg(test)]
 mod test {
